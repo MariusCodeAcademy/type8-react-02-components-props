5808462c58db5fc59108b57ccf65ebf5c3167fbb
Marius <marius@example.com>
2022-05-31 11:35:57 +0300

    pass obj as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import img1 from './assets/newyork.jpg';
 import parisImg from './assets/paris.jpg';
+import Card from './components/Card/Card';
 import CardContent from './components/Card/CardContent';
 import CardImage from './components/Card/CardImage';
 import Icon from './components/UI/Icon';
 import MyBtn from './components/UI/MyBtn';
 
 const card1Data = {
-  title: 'newyork',
+  title: 'New York',
   image: img1,
   date: '2022-05-15',
   text: 'Heloo from new york',
@@ -22,22 +23,25 @@ function App() {
       <h1 className='main-title'>Heloo again world</h1>
       <Icon icon='fa-facebook-official' />
       <Icon icon='fa-instagram' />
-      <article className='card'>
-        <CardImage img={img1} />
-        <CardContent
-          title='New York'
-          date='Tue May 31 2022'
-          text='Welcome to new York, have a great time'
-        />
-      </article>
-      <article className='card'>
-        <CardImage img={parisImg} />
-        <CardContent
-          title='Paris'
-          date='Tue May 31 2019'
-          text='Welcome to Paris, eat a baguet'
-        />
-      </article>
+      <div className='card-container'>
+        <article className='card'>
+          <CardImage img={img1} />
+          <CardContent
+            title='New York'
+            date='Tue May 31 2022'
+            text='Welcome to new York, have a great time'
+          />
+        </article>
+        <article className='card'>
+          <CardImage img={parisImg} />
+          <CardContent
+            title='Paris'
+            date='Tue May 31 2019'
+            text='Welcome to Paris, eat a baguet'
+          />
+        </article>
+        <Card data={card1Data} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import CardContent from './CardContent';
+import CardImage from './CardImage';
+
+function Card(props) {
+  console.log('props ===', props);
+  return (
+    <article className='card'>
+      <CardImage img={props.data.image} />
+      <CardContent
+        title={props.data.title}
+        date={props.data.date}
+        text={props.data.text}
+      />
+    </article>
+  );
+}
+
+export default Card;
