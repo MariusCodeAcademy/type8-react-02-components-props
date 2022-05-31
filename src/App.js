fc4578eac6a4f1b476a46ffcbc6bc7c815ede51f
Marius <marius@example.com>
2022-05-31 09:59:36 +0300

    CardContent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import img1 from './newyork.jpg';
+import img1 from './assets/newyork.jpg';
+import parisImg from './assets/paris.jpg';
+import CardImage from './components/Card/CardImage';
 import MyBtn from './components/UI/MyBtn';
+
+const card1Data = {
+  title: 'newyork',
+  image: img1,
+  date: '2022-05-15',
+  text: 'Heloo from new york',
+};
 
 function App() {
   return (
     <div className='App'>
       <MyBtn text='I am reusable component' />
+      {/* <CardImage img={parisImg} /> */}
       <h1 className='main-title'>Heloo again world</h1>
       <article className='card'>
-        <img className='card-image' src={img1} alt='Card' />
+        <CardImage img={img1} />
+        {/* <CardContent title date text /> */}
         <div className='card-content'>
           <h3>New york</h3>
           <p className='date'>{new Date().toDateString()}</p>
