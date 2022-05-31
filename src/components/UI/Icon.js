cecee8a1009ec79a1276cc81f5943f45626ef850
Marius <marius@example.com>
2022-05-31 10:33:01 +0300

    icon uzd

---
+++ src/components/UI/Icon.js
@@ -0,0 +1,4 @@
+// create and export Icon component
+// componet returns <i class="fa fa-facebook-official" aria-hidden="true"></i>
+// make it so prop icon=fa-facebook-official loads icon
+// use icon in App.js
--- src/components/UI/MyBtn.js
+++ src/components/UI/MyBtn.js
@@ -1,7 +1,7 @@
 function MyBtn(props) {
-  console.log('MyBtn mounted');
+  console.log('MyBtn');
   console.log('props ===', props);
-  return <button className='btn-black'>{props.text}</button>;
+  return <button className={'btn-black'}>{props.text}</button>;
 }
 
 export default MyBtn;
